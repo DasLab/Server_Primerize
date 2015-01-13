d628a9b1e36a75035cd158334e905f09bfc9d895
T47 <t47@stanford.edu>
2015-01-13 12:39:53 -0800

    simplify scroll effect

--- res/js/tutorial.js
+++ res/js/tutorial.js
@@ -1,31 +1,6 @@
 $(document).ready(function () {
-  $('#up').on('click', function (e) {
-    e.preventDefault();
-    $('html, body').stop().animate({scrollTop: $('#down').offset().top}, 500);
-  });
-  $('#tab_1').on('click', function (e) {
-    e.preventDefault();
-    $('html, body').stop().animate({scrollTop: $('#web_input').offset().top}, 500);
-  });
-  $('#tab_2').on('click', function (e) {
-    e.preventDefault();
-    $('html, body').stop().animate({scrollTop: $('#web_output_primers').offset().top}, 500);
-  });
-  $('#tab_3').on('click', function (e) {
-    e.preventDefault();
-    $('html, body').stop().animate({scrollTop: $('#web_output_assembly').offset().top}, 500);
-  });
-  $('#tab_4').on('click', function (e) {
-    e.preventDefault();
-    $('html, body').stop().animate({scrollTop: $('#txt_output').offset().top}, 500);
-  });
-  $('#tab_5').on('click', function (e) {
-    e.preventDefault();
-    $('html, body').stop().animate({scrollTop: $('#idt_bulk').offset().top}, 500);
-  });
-  $('#tab_6').on('click', function (e) {
-    e.preventDefault();
-    $('html, body').stop().animate({scrollTop: $('#PCR').offset().top}, 500);
+  $('[id^=tab_], #up').on('click', function () {
+    $('html, body').stop().animate({scrollTop: $($(this).attr("href")).offset().top - 50}, 500);
   });
 
   $("#btn_demo").on('click', function () {
