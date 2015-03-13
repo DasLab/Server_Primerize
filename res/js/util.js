0c8ff8ee1d3bfffed9a4d919d6567882578c589e
T47 <t47@stanford.edu>
2015-03-12 17:22:32 -0700

    fix image links

--- res/js/util.js
+++ res/js/util.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
   var str_title = "Primerize: Primer Design for PCR Assembly";
   var path_icon = "/res/images/ico_primerize.png?";
 
-  var path_logo_primerize = "/res/images/logo_primerize.png?";
-  var path_logo_das = "/res/images/logo_das.jpg?";
-  var path_logo_stanford = "/res/images/logo_stanford.png?";
-  var path_logo_eterna = "/res/images/logo_eterna.png?";
-  var path_background_rna = "/res/images/bg_rna.png?";
-  var path_button_loading = "/res/images/fg_load.gif?";
-  var path_button_top = "/res/images/fg_top.png?";
-  var path_button_question = "/res/images/fg_question.png?";
+  var path_logo_primerize = "/res/images/logo_primerize.png";
+  var path_logo_das = "/res/images/logo_das.jpg";
+  var path_logo_stanford = "/res/images/logo_stanford.png";
+  var path_logo_eterna = "/res/images/logo_eterna.png";
+  var path_background_rna = "/res/images/bg_rna.png";
+  var path_button_loading = "/res/images/fg_load.gif";
+  var path_button_top = "/res/images/fg_top.png";
+  var path_button_question = "/res/images/fg_question.png";
 
   var path_home = "/home";
   var path_design = "/design";
