aa92de535dd9a36500d6e30461e5b96a3a49a081
T47 <t47@stanford.edu>
2015-04-30 11:53:49 -0700

    resize trigger for tutorial

--- res/js/tutorial.js
+++ res/js/tutorial.js
@@ -1,10 +1,22 @@
-$(document).ready(function () {
+function resize() {
+  $("#main").addClass("pull-right");
   $("#sidebar").css("width", $("#navbar").width() - $("#main").width() - 30);
   $("#sidebar").affix({
         offset: {
           top: $("#main").position().top
         }
   }); 
+  if ($("#navbar").width() > 1500) {
+    $("#main").removeClass("pull-right");
+  } else {
+    $("#main").addClass("pull-right");
+  }
+  $("#sidebar").css("max-width", 300);  
+}
+
+
+$(document).ready(function () {
+  resize();
 
   $('[id^=tab_], #up').on('click', function () {
     $('html, body').stop().animate({scrollTop: $($(this).attr("href")).offset().top - 75}, 500);
@@ -22,3 +34,9 @@ $(window).on("scroll", function () {
 		$('#up').fadeOut();
 	}
 });
+
+$(window).on("resize", function() {
+  clearTimeout($.data(this, 'resizeTimer'));
+  $.data(this, 'resizeTimer', setTimeout(resize(), 200));
+});
+
