eb1b7388aed6afac88b707c95f6af64ef8636d15
T47 <t47@stanford.edu>
2015-04-30 17:01:01 -0700

    500/100 input length pop out

--- res/js/design.js
+++ res/js/design.js
@@ -10,13 +10,28 @@ $(document).ready(function () {
   //       $(this).parent().css("color","red");
   //   }
   // });
+  $("#warn_500, #warn_1000").css("display", "none");
 
   $("#sequence").on("keyup", function () {
     $("#count").text($(this).val().length);
     if ($(this).val().length < 60) {
-        $("#count").parent().parent().css("color","red");
+        $("#count").parent().parent().css({"color":"red", "background-color":"white"});
+        $("#warn_500, #warn_1000").css("display", "none");
     } else {
-        $("#count").parent().parent().css("color","black");
+        $("#count").parent().parent().css({"color":"black", "background-color":"white"});
+        if ($(this).val().length > 500) {
+          if ($(this).val().length > 1000) {
+            $("#count").parent().parent().css({"color":"red", "background-color":"black"});
+            $("#warn_1000").css("display", "inline-block");
+            $("#warn_500").css("display", "none");
+          } else {
+            $("#count").parent().parent().css({"color":"orange", "background-color":"black"});
+            $("#warn_500").css("display", "inline-block");
+            $("#warn_1000").css("display", "none");
+          }
+        } else {
+          $("#warn_500, #warn_1000").css("display", "none");
+        }
     }
   });
 
@@ -32,6 +47,7 @@ $(document).ready(function () {
     var job_id = Math.random().toString(16).substring(2, 15) + Math.random().toString(16).substring(2, 15);
     $("#job_id").val(job_id.toString());
     $("#modal_id").text(job_id.toString());
+    $("#url_id").text('http://primerize.stanford.edu/result?job_id='.concat(job_id.toString()));
 
     $("#modal_wait").modal("show");
     $("#wait").fadeIn(1000);
