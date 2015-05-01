459da6aa6190c17e1fee871c9902fec385d2cb7c
T47 <t47@stanford.edu>
2015-04-30 17:00:40 -0700

    add demo for wait/fail/error cases

--- const.py
+++ const.py
@@ -17,6 +17,11 @@ PATH_ABOUT = "res/html/about.html"
 PATH_404 = "res/html/_404.html"
 PATH_500 = "res/html/_500.html"
 
+PATH_DEMO_ERROR = "res/html/example_error.html"
+PATH_DEMO_FAIL = "res/html/example_fail.html"
+PATH_DEMO_WAIT = "res/html/example_wait.html"
+
+
 QUICKSTART_CONFIG = {
         "/res/css": {
             "tools.staticdir.on": True,
