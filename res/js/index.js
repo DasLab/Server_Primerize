e7c0e816ed5a5d4e2b1580923abdc6fcaae9521f
T47 <t47@stanford.edu>
2015-05-02 17:14:39 -0700

    fix home page job_id form

---
+++ res/js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+  var col_h = Math.max(parseInt($("#col-1").css("height")), parseInt($("#col-2").css("height")), parseInt($("#col-3").css("height")), parseInt($("#col-4").css("height")));
+  $("#col-1").css("height", col_h);
+  $("#col-2").css("height", col_h);
+  $("#col-3").css("height", col_h);
+  $("#col-4").css("height", col_h);
+
+  $("#btn_retrieve").on("click", function () { $("#wait").fadeIn(1000); });
+  $("#btn_demo").on("click", function () { $("#wait").fadeIn(1000); });
+  
+});
+
+
+
