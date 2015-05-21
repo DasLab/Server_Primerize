e9ddb5a8119a27aef36cc272354b93ea007a8837
T47 <t47@stanford.edu>
2015-05-21 12:58:00 -0700

    add glyphicon to design, update demos

--- run_server.py
+++ run_server.py
@@ -80,7 +80,7 @@ class Root:
                 sequence += char
         if len(sequence) < 60 or not is_valid_sequence(sequence):
             if not sequence:
-                return Root.design(self)
+                return self.design_1d()
 
             if len(sequence) < 60:
                 msg = "Invalid sequence input (should be <u>at least <b>60</b> nt</u> long)."
@@ -127,11 +127,11 @@ class Root:
                 create_err_html(sequence, tag, min_Tm, num_primers, max_length, min_length, is_num_primers, is_t7, job_id)
                 raise cherrypy.HTTPRedirect("result?job_id=%s" % job_id)
 
-            # time.sleep(5)
+            # time.sleep(15)
 
             # when no solution found
             if lines[-2] and lines[-2][0] == "?":
-                msg = "<br/><hr/><div class=\"container theme-showcase\"><div class=\"row\"><div class=\"col-md-8\"><h2>Output Result:</h2></div><div class=\"col-md-4\"><h4 class=\"text-right\"><span class=\"label label-violet\">JOB_ID</span>: <span class=\"label label-inverse\">__JOB_ID___</span></h4><a href=\"__FILE_NAME__\" class=\"btn btn-blue pull-right\" style=\"color: #ffffff;\" title=\"Output in plain text\" download disabled>&nbsp;Save Result&nbsp;</a></div></div><br/><div class=\"alert alert-danger\"><p><b>FAILURE</b>: No solution found (Primerize run finished without errors).<br/><ul><li>Please examine the advanced options. Possible solutions might be restricted by stringent options combination, especially by minimum Tm and # number of primers. Try again with relaxed the advanced options.</li><li>Certain input sequence, e.g. polyA or large repeats, might be intrinsically difficult for PCR assembly design.</li><li>For further information, please feel free to <a class=\"btn btn-warning btn-sm path_about\" href=\"#contact\" style=\"color: #ffffff;\">Contact</a> us to track down the problem.</li></ul></p></div>"
+                msg = "<br/><hr/><div class=\"container theme-showcase\"><div class=\"row\"><div class=\"col-md-8\"><h2>Output Result:</h2></div><div class=\"col-md-4\"><h4 class=\"text-right\"><span class=\"glyphicon glyphicon-search\"></span>&nbsp;&nbsp;<span class=\"label label-violet\">JOB_ID</span>: <span class=\"label label-inverse\">__JOB_ID___</span></h4><a href=\"__FILE_NAME__\" class=\"btn btn-blue pull-right\" style=\"color: #ffffff;\" title=\"Output in plain text\" download disabled>&nbsp;Save Result&nbsp;</a></div></div><br/><div class=\"alert alert-danger\"><p><span class=\"glyphicon glyphicon-minus-sign\"></span>&nbsp;&nbsp;<b>FAILURE</b>: No solution found (Primerize run finished without errors).<br/><ul><li>Please examine the advanced options. Possible solutions might be restricted by stringent options combination, especially by minimum Tm and # number of primers. Try again with relaxed the advanced options.</li><li>Certain input sequence, e.g. polyA or large repeats, might be intrinsically difficult for PCR assembly design.</li><li>For further information, please feel free to <a class=\"btn btn-warning btn-sm path_about\" href=\"#contact\" style=\"color: #ffffff;\">Contact</a> us to track down the problem.</li></ul></p></div>"
                 msg = msg.replace("__JOB_ID___", job_id).replace("__FILE_NAME__", "/cache/result_%s.txt" % job_id)
                 html_content = html_content.replace("__RESULT__", msg)
                 create_res_html(html_content, job_id)
@@ -145,10 +145,10 @@ class Root:
 
                 script = ""
                 if self.lines_warning != ['#']:
-                    script += "<br/><hr/><div class=\"container theme-showcase\"><div class=\"row\"><div class=\"col-md-8\"><h2>Output Result:</h2></div><div class=\"col-md-4\"><h4 class=\"text-right\"><span class=\"label label-violet\">JOB_ID</span>: <span class=\"label label-inverse\">__JOB_ID___</span></h4><a href=\"__FILE_NAME__\" class=\"btn btn-blue pull-right\" style=\"color: #ffffff;\" title=\"Output in plain text\" download>&nbsp;Save Result&nbsp;</a></div></div><br/><div class=\"alert alert-warning\" title=\"Mispriming alerts\"><p>"
+                    script += "<br/><hr/><div class=\"container theme-showcase\"><div class=\"row\"><div class=\"col-md-8\"><h2>Output Result:</h2></div><div class=\"col-md-4\"><h4 class=\"text-right\"><span class=\"glyphicon glyphicon-search\"></span>&nbsp;&nbsp;<span class=\"label label-violet\">JOB_ID</span>: <span class=\"label label-inverse\">__JOB_ID___</span></h4><a href=\"__FILE_NAME__\" class=\"btn btn-blue pull-right\" style=\"color: #ffffff;\" title=\"Output in plain text\" download>&nbsp;Save Result&nbsp;</a></div></div><br/><div class=\"alert alert-warning\" title=\"Mispriming alerts\"><p>"
                     for line in self.lines_warning:
                         if line[0] == "@":
-                            script += "<b>WARNING</b>"
+                            script += "<span class=\"glyphicon glyphicon-exclamation-sign\"></span>&nbsp;&nbsp;<b>WARNING</b>"
                             for char in line[8:]:
                                 if char == "F":
                                     script += "</b><span class=\"label label-info\">"
@@ -169,14 +169,14 @@ class Root:
                                 else:
                                     script += char 
                             script += "<br/>"
-                    script += "<b>WARNING</b>: One-pot PCR assembly may fail due to mispriming; consider first assembling fragments in a preliminary PCR round (subpool).<br/>"
+                    script += "<span class=\"glyphicon glyphicon-info-sign\"></span>&nbsp;&nbsp;<b>WARNING</b>: One-pot PCR assembly may fail due to mispriming; consider first assembling fragments in a preliminary PCR round (subpool).<br/>"
                 else:
                     script += "<div class=\"container theme-showcase\"><div class=\"row\"><div class=\"col-md-8\"><h2>Output Result:</h2></div><div class=\"col-md-4\"><h4 class=\"text-right\"><span class=\"label label-violet\">JOB_ID</span>: <span class=\"label label-inverse\">__JOB_ID___</span></h4><a href=\"__FILE_NAME__\" class=\"btn btn-blue pull-right\" title=\"Output in plain text\" download>&nbsp;Download&nbsp;</a></div></div><br/><div class=\"alert alert-success\" title=\"No alerts\"><p>"
-                    script += "<b>SUCCESS</b>: No potential mis-priming found. See results below.<br/>"
+                    script += "<span class=\"glyphicon glyphicon-ok-sign\"></span>&nbsp;&nbsp;<b>SUCCESS</b>: No potential mis-priming found. See results below.<br/>"
 
-                script += "</p></div><div class=\"row\"><div class=\"col-md-10\"><div class=\"alert alert-default\"><p>__NOTE_T7__</p></div></div><div class=\"col-md-2\"><div class=\"alert alert-orange text-center\"> <b>Time elapsed</b>:<br/><i>%.1f</i> s.</div></div></div>" % t_total
+                script += "</p></div><div class=\"row\"><div class=\"col-md-10\"><div class=\"alert alert-default\"><p>__NOTE_T7__</p></div></div><div class=\"col-md-2\"><div class=\"alert alert-orange text-center\"> <span class=\"glyphicon glyphicon-time\"></span>&nbsp;&nbsp;<b>Time elapsed</b>:<br/><i>%.1f</i> s.</div></div></div>" % t_total
 
-                script += "<div class=\"row\"><div class=\"col-md-12\"><div class=\"panel panel-primary\"><div class=\"panel-heading\"><h2 class=\"panel-title\">Designed Primers</h2></div><div class=\"panel-body\"><table class=\"table table-hover\" ><thead><tr><th class=\"col-md-1\">#</th><th class=\"col-md-1\">Length</th><th class=\"col-md-10\">Sequence</th></tr></thead><tbody>"
+                script += "<div class=\"row\"><div class=\"col-md-12\"><div class=\"panel panel-primary\"><div class=\"panel-heading\"><h2 class=\"panel-title\"><span class=\"glyphicon glyphicon-indent-left\"></span>&nbsp;&nbsp;Designed Primers</h2></div><div class=\"panel-body\"><table class=\"table table-hover\" ><thead><tr><th class=\"col-md-1\">#</th><th class=\"col-md-1\">Length</th><th class=\"col-md-10\">Sequence</th></tr></thead><tbody>"
                 for line in self.lines_primers:
                     line = line.split("\t")
                     num = "<b>" + line[0][7:]
@@ -186,7 +186,7 @@ class Root:
                         num = "<tr class=\"warning\"><td>" + num + "<span class=\"label label-info\">F</span></b>"
                     script += num + "</td><td><em>" + line[1] + "</em></td><td style=\"word-break: break-all;\">" + line[2] + "</td></tr>"
 
-                script += "</tbody></table></div></div></div></div><div class=\"row\"><div class=\"col-md-12\"><div class=\"panel panel-green\"><div class=\"panel-heading\"><h2 class=\"panel-title\">Assembly Scheme</h2></div><div class=\"panel-body\"><pre>"
+                script += "</tbody></table></div></div></div></div><div class=\"row\"><div class=\"col-md-12\"><div class=\"panel panel-green\"><div class=\"panel-heading\"><h2 class=\"panel-title\"><span class=\"glyphicon glyphicon-tasks\"></span>&nbsp;&nbsp;Assembly Scheme</h2></div><div class=\"panel-body\"><pre>"
                 for line in self.lines_assembly:
                     if line:
                         if line[0] == "~":
@@ -198,14 +198,20 @@ class Root:
                                 if char in SEQ['valid']:
                                     script += "<span class=\"label-info\">" + char + "</span>"
                                 else:
-                                    script += char
+                                    if char.isdigit():
+                                        script += '<b> ' + char + '</b>'
+                                    else:
+                                        script += char
                             script += "<br/>"
                         elif line[0] == "!":
                             for char in line[1:]:
                                 if char in SEQ['valid']:
                                     script += "<span class=\"label-white label-danger\">" + char + "</span>"
                                 else:
-                                    script += char
+                                    if char.isdigit():
+                                        script += '<b>' + char + ' </b>'
+                                    else:
+                                        script += char
                             script += "<br/>"
                         else:
                             for char in line[1:]:
@@ -219,7 +225,9 @@ class Root:
                     else:
                         script += "<br/>"
 
-                script += "</pre></div></div></div></div><p class=\"lead\"><b><u><i>What next?</i></u></b> Try our suggested experimental <a class=\"btn btn-info btn-sm path_protocol\" href=\"\" role=\"button\" style=\"color: #ffffff;\">&nbsp;&nbsp;Protocol&nbsp;&nbsp;</a> for PCR assembly.</p> </div>"
+                script += "</pre></div></div></div></div><p class=\"lead\"><span class=\"glyphicon glyphicon-question-sign\"></span>&nbsp;&nbsp;<b><u><i>What next?</i></u></b> Try our suggested experimental <a class=\"btn btn-info btn-sm path_protocol\" href=\"\" role=\"button\" style=\"color: #ffffff;\">&nbsp;&nbsp;Protocol&nbsp;&nbsp;</a> for PCR assembly.</p> </div>"
+
+                script = script.replace('->', '<span class=\"label-orange label-white glyphicon glyphicon-circle-arrow-right\"></span>').replace('<-', '<span class=\"label-green label-white glyphicon glyphicon-circle-arrow-left\"></span>')
 
                 # f = tempfile.NamedTemporaryFile(mode="w+b", prefix="result_", suffix=".txt", dir="cache", delete=False)
                 # job_id = binascii.b2a_hex(os.urandom(7)) #f.name[-17:]
@@ -234,15 +242,15 @@ class Root:
                     f.write("NUM_PRIMERS: %d" % num_primers)
                 f.write("\nMAX_LENGTH: %d\nMIN_LENGTH: %d\n" % (max_length, min_length))
                 if "1" in is_t7:
-                    str_t7 = "T7_CHECK: feature enabled (uncheck the option to disable). T7 promoter sequence "
+                    str_t7 = "<span class=\"glyphicon glyphicon-plus-sign\"></span>&nbsp;&nbsp;T7_CHECK: feature enabled (uncheck the option to disable). T7 promoter sequence "
                     if flag:
                         str_t7 = str_t7 + "is present, no action was taken.\n"
                     else:
                         str_t7 = str_t7 + "was absent, Primerize automatically prepended it. \n"
                     if is_G:
-                        str_t7 += "SUCCESS: T7 promoter sequence is followed by nucleotides GG.\n"
+                        str_t7 += "<span class=\"glyphicon glyphicon-ok-sign\"></span>&nbsp;&nbsp;SUCCESS: T7 promoter sequence is followed by nucleotides GG.\n"
                     else:
-                        str_t7 += "WARNING: T7 promoter sequence is NOT followed by nucleotides GG. Consider modifying the sequence for better transcription.\n"
+                        str_t7 += "<span class=\"glyphicon glyphicon-exclamation-sign\"></span>&nbsp;&nbsp;WARNING: T7 promoter sequence is NOT followed by nucleotides GG. Consider modifying the sequence for better transcription.\n"
                 else:
                     str_t7 = "T7_CHECK: feature disabled (check the option to enable). No checking was performed.\n"
                 f.write(str_t7.replace("SUCCESS","T7_CHECK").replace("WARNING","T7_CHECK"))
