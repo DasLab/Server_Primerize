591f2e8c81c3263550539d834d8a0472b4025c0a
T47 <t47@stanford.edu>
2015-05-21 13:48:40 -0700

    add google analytics tracker

--- const.py
+++ const.py
@@ -3,6 +3,8 @@ import subprocess
 def get_jquery_ver():
     return subprocess.Popen('ls res/js/jquery-*.min.js', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].replace('res/js/jquery-', '').replace('.min.js', '').strip()
 
+GA_TRACKER = "<script type=\"text/javascript\">(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-36037648-2', 'auto'); ga('send', 'pageview'); </script>"
+
 
 ARG = {
     'DEF_MIN_TM': 60.0,
