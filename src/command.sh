b66f448f083c115b7495ed49c243a86d74e84a60
T47 <t47@stanford.edu>
2015-05-25 21:16:30 -0700

    admin misc writeup

--- src/command.sh
+++ src/command.sh
@@ -3,9 +3,14 @@
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to 8080
 
 # run screen
-screen -dmL python run_server.py release
+screen 
+# press [enter]
+source ~/.bash_profile
+python run_server.py release
+# press [ctrl+A+D]
+
 # kill screen
 screen -ls
 screen -S *** -X kill
 # reattach screen
-screen -dr
+screen -dr ***
