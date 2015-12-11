4d8f6777294d22c7b4f93fd0dcca58489a37bc36
T47 <t47@stanford.edu>
2015-12-10 18:33:28 -0800

    minor icons, add 1d->2d ad

--- media/js/public/tutorial.js
+++ media/js/public/tutorial.js
@@ -40,8 +40,6 @@ $(document).ready(function () {
   $('[id^=tab_], #up').on('click', function () {
     $('html, body').stop().animate({scrollTop: $($(this).attr("href")).offset().top - 75}, 500);
   });
-
-  $("#btn_demo").on('click', function () { $("#modal_demo").modal("show"); });
 });
 
 $(window).on("scroll", function () {
