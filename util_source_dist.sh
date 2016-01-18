aa5dab19a9c265a51a43134336336485291eab35
T47 <t47@stanford.edu>
2016-01-18 12:56:39 -0800

    fix source_dist path

--- util_source_dist.sh
+++ util_source_dist.sh
@@ -1,7 +1,9 @@
 cd ~
 git clone https://github.com/DasLab/Primerize
 sudo cp -r Primerize/* NA_Thermo/
+cd ~/NA_Thermo
 sudo python setup.py install
 
+cd ~
 zip -rm primerize_release.zip Primerize/
 sudo mv primerize_release.zip ~/Server_Primerize/data/
