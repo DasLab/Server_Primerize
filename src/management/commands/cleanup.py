1a7035f61f7b7d29a4de2ba764eae469fe4c8165
T47 <t47@stanford.edu>
2016-10-22 17:21:17 -0700

    add arg for custom cleanup days

--- src/management/commands/cleanup.py
+++ src/management/commands/cleanup.py
@@ -3,7 +3,6 @@ import glob
 import os
 import sys
 import time
-import traceback
 
 from django.core.management.base import BaseCommand
 
@@ -15,19 +14,22 @@ from src.console import send_notify_emails
 class Command(BaseCommand):
     help = 'Cleans up old job results from database and folders.'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--days', type=int, help='Days in the past considered obsolete, default 3 month is 90.')
+
     def handle(self, *args, **options):
         t0 = time.time()
         self.stdout.write('%s:\t%s' % (time.ctime(), ' '.join(sys.argv)))
+        N_days = options['days'] if options['days'] else KEEP_JOB * 30
 
-        flag = False
         t = time.time()
         self.stdout.write("Cleaning up obsolete job results...")
 
-        all_job = JobIDs.objects.filter(date__range=(datetime.date(1970, 1, 2), datetime.date.today() - datetime.timedelta(days=KEEP_JOB * 30)))
+        all_job = JobIDs.objects.filter(date__range=(datetime.date(1970, 1, 2), datetime.date.today() - datetime.timedelta(days=N_days)))
         N_obsolete = 0
 
-        try:
-            for job in all_job:
+        for job in all_job:
+            try:
                 if job.type == '1':
                     obj = Design1D.objects.get(job_id=job.job_id)
                 elif job.type == '2':
@@ -38,16 +40,12 @@ class Command(BaseCommand):
                 obj.delete()
                 for f in glob.glob('%s/data/%sd/result_%s.*') % (MEDIA_ROOT, job.type, job.job_id):
                     os.remove(f)
-                job.delete()
-                N_obsolete += 1
+            except Exception:
+                pass
 
-        except Exception:
-            self.stdout.write("    \033[41mERROR\033[0m: Failed to remove JOB_ID \033[94m%s\033." % job.job_id)
-            err = traceback.format_exc()
-            ts = '%s\t\t%s\n' % (time.ctime(), ' '.join(sys.argv))
-            open('%s/cache/log_alert_admin.log' % MEDIA_ROOT, 'a').write(ts)
-            open('%s/cache/log_cron_cleanup.log' % MEDIA_ROOT, 'a').write('%s\n%s\n' % (ts, err))
-            flag = True
+            job.delete()
+            N_obsolete += 1
+
 
         all_files = set()
         N_orphan = 0
@@ -71,25 +69,18 @@ class Command(BaseCommand):
                 try:
                     job = JobIDs.objects.get(job_id=job_id)
                     job.delete()
+                    N_orphan += 1
                 except Exception:
-                    self.stdout.write("    \033[41mERROR\033[0m: Failed to remove JOB_ID \033[94m%s\033." % job_id)
+                    pass
 
-                N_orphan += 1
+        self.stdout.write("    \033[92mSUCCESS\033[0m: \033[94m%s\033[0m obsolete job result files removed." % N_obsolete)
+        self.stdout.write("    \033[92mSUCCESS\033[0m: \033[94m%s\033[0m orphan job result files removed." % N_orphan)
+        self.stdout.write("Time elapsed: %.1f s.\n" % (time.time() - t))
 
+        if not DEBUG:
+            t_now = datetime.datetime.now().strftime('%b %d %Y (%a) @ %H:%M:%S')
+            send_notify_emails('{%s} SYSTEM: Quarterly Cleanup Notice' % env('SERVER_NAME'), 'This is an automatic email notification for the success of scheduled quarterly cleanup of the %s Server local results.\n\nThe crontab job is scheduled at 00:00 (UTC) on 1st day of every 3 months.\n\nThe last system backup was performed at %s (PDT).\n\n%s Admin\n' % (env('SERVER_NAME'), t_now, env('SERVER_NAME')))
+            self.stdout.write("Admin email (Quarterly Cleanup Notice) sent.")
 
-        if flag:
-            self.stdout.write("Finished with errors!")
-            self.stdout.write("Time elapsed: %.1f s." % (time.time() - t0))
-            sys.exit(1)
-        else:
-            self.stdout.write("    \033[92mSUCCESS\033[0m: \033[94m%s\033[0m obsolete job result files removed." % N_obsolete)
-            self.stdout.write("    \033[92mSUCCESS\033[0m: \033[94m%s\033[0m orphan job result files removed." % N_orphan)
-            self.stdout.write("Time elapsed: %.1f s.\n" % (time.time() - t))
-
-            if not DEBUG:
-                t_now = datetime.datetime.now().strftime('%b %d %Y (%a) @ %H:%M:%S')
-                send_notify_emails('{%s} SYSTEM: Quarterly Cleanup Notice' % env('SERVER_NAME'), 'This is an automatic email notification for the success of scheduled quarterly cleanup of the %s Server local results.\n\nThe crontab job is scheduled at 00:00 (UTC) on 1st day of every 3 months.\n\nThe last system backup was performed at %s (PDT).\n\n%s Admin\n' % (env('SERVER_NAME'), t_now, env('SERVER_NAME')))
-                self.stdout.write("Admin email (Quarterly Cleanup Notice) sent.")
-
-            self.stdout.write("All done successfully!")
-            self.stdout.write("Time elapsed: %.1f s." % (time.time() - t0))
+        self.stdout.write("All done successfully!")
+        self.stdout.write("Time elapsed: %.1f s." % (time.time() - t0))
