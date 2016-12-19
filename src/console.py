8b3e2c36ba17b8eb782cf5bd2ff8a12218b67734
t47io <t47@alumni.stanford.edu>
2016-12-18 19:28:50 -0800

    add extra admin email

--- src/console.py
+++ src/console.py
@@ -37,7 +37,8 @@ def send_notify_emails(msg_subject, msg_content):
     smtpserver.starttls()
     smtpserver.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
     msg = 'Subject: %s\n\n%s' % (msg_subject, msg_content)
-    smtpserver.sendmail(EMAIL_HOST_USER, EMAIL_NOTIFY, msg)
+    for recipient in EMAIL_NOTIFY:
+        smtpserver.sendmail(EMAIL_HOST_USER, recipient, msg)
     smtpserver.quit()
 
 
